9d91d4f25d54ebffe4ce9fb68b7241f8b20bde36
Jeevanandhan01 <jeevanandhanofficial@gmail.com>
2024-01-30 01:45:48 +0530

    Updated

--- js/custom.js
+++ js/custom.js
@@ -33,8 +33,8 @@ $(".owl-carousel").owlCarousel({
 /** google_map js **/
 function myMap() {
     var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
+        center: new google.maps.LatLng(11.0168, 76.9558),
+        zoom: 10,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
